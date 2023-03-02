15a137e05994677fe495a080c73b7316c94d9822
Zed Grimm <74928681+Calastrophe@users.noreply.github.com>
2023-03-02 11:49:27 -0600

    more planning, arch and mode fleshed out

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,46 @@
-use unicorn_engine::{RegisterX86, Unicorn};
+use unicorn_engine::Unicorn;
 use unicorn_engine::unicorn_const::{Arch, Mode, Permission};
-use clap::Parser;
-use object::{Object,ObjectSection};
+use object::{Object,ObjectSection, Architecture};
 use std::error::Error;
 use std::fs;
 
-// NOTE: This file is acting like the library for the emulator
-
-pub const ARG_TABLE: [RegisterX86; 6] = [RegisterX86::RDI, RegisterX86::RSI, RegisterX86::RDX, RegisterX86::RCX, RegisterX86::R8, RegisterX86::R9];
-pub const REG_TABLE: [RegisterX86; 7] = [RegisterX86::RAX, RegisterX86::RDI, RegisterX86::RSI, RegisterX86::RDX, RegisterX86::RCX, RegisterX86::R8, RegisterX86::R9];
-
+// NOTE: This file is acting like the emulation library for the interface.
 
 // A context which is created each time a new file is loaded.
 pub struct Context<'a> {
     uc: Unicorn<'a, ()>, // Architecture and mode will be found here.
     file: object::File<'a> // The file which is associated with this new context
 }
+
+impl<'a> Context<'a> {
+    // Only x86_x64, ARM, ARM64 supported at the moment.
+    fn get_arch_and_mode(&self) -> Option<(Arch, Mode)> {
+        match self.file.architecture() {
+            Architecture::Aarch64 => Some((Arch::ARM64, Mode::ARM)),
+            Architecture::Arm => Some((Arch::ARM, Mode::ARM)),
+            Architecture::X86_64 => Some((Arch::X86, Mode::MODE_64)),
+            Architecture::X86_64_X32 => Some((Arch::X86, Mode::MODE_32)),
+            _ => None
+        }
+    }
+
+    // Generate function signatures to display in listing
+    // Try to use debug symbols to determine signature first
+    fn generate_function_sigs(&self) {
+        return;
+    }
+
+    pub fn new(filename: String) -> Self {
+        // Parse the given filename and parse into a File object
+
+        // Create a context with retrieved arch and mode
+
+        // Map each segment into the context's unicorn instance
+
+        
+    }
+}
+
 
 
 
